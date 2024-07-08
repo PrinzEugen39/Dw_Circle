e6fc7c1bf4adde535fb99039964ac9e9549e403b
PrinzEugen39 <habibsan01@gmail.com>
2024-07-08 12:45:46 +0700

    chore: Update npm start command to include port number

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-undef */
-module.exports = {
-  apps: [
-    {
-      name: "Circle FE",
-      script: "npm",
-      args: "start -- -p 3903",
-      watch: true,
-    },
-  ],
-};
+export const apps = [
+  {
+    name: "Circle FE",
+    script: "npm",
+    args: "start -- -p 3903",
+    watch: true,
+  },
+];
